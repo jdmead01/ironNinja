34f31cb2c2ebd2de23eaee18c9f438d71fe7e856
Jimmy Mead <jdmead@asu.edu>
2019-08-14 08:22:10 -0700

    first commit

---
+++ SpiceHound.cs
@@ -0,0 +1,8 @@
+class SpiceHound : Ninja
+{
+    // provide override for IsFull (Full at 1200 Calories)
+    public override void Consume(IConsumable item)
+    {
+        // provide override for Consume
+    }
+}
---
+++ SweetTooth.cs
@@ -0,0 +1,8 @@
+class SweetTooth : Ninja
+{
+    // provide override for IsFull (Full at 1500 Calories)
+    public override void Consume(IConsumable item)
+    {
+        // provide override for Consume
+    }
+}
